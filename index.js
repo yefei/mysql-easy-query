129188638cbba19566406578f61e1cbb53ba42fe
YeFei <316606233@qq.com>
2021-01-21 14:35:53 +0800

    export Builder

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const debug = require('./lib/debug');
 const Query = require('./lib/query');
 const PoolQuery = require('./lib/pool_query');
 const PoolClusterQuery = require('./lib/pool_cluster_query');
+const { Builder } = require('sql-easy-builder');
 
 module.exports = {
   debug,
   Query,
   PoolQuery,
   PoolClusterQuery,
+  Builder,
 };
