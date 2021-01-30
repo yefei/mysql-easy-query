84a7f276c490d1eed63e09797621b2bd8a83ddd3
Fei Ye <316606233@qq.com>
2021-01-30 11:03:56 +0800

    bugfix: query one no result undefined

--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('dotenv').config();
+
 const assert = require('assert');
 const mysql = require('mysql');
 const { Query, PoolQuery } = require('..');
@@ -21,6 +23,7 @@ const pool = mysql.createPool({
   host: '127.0.0.1',
   user: 'root',
   database: 'test',
+  password: process.env.MYSQL_PASSWORD,
 });
 
 let conn;
@@ -45,39 +48,45 @@ after(function() {
 });
 
 function eq(a, b) {
-  assert.deepStrictEqual(Object.assign({}, a), b);
+  assert.deepStrictEqual(Array.isArray(a) ? a.map(i => Object.assign({}, i)) : Object.assign({}, a), b);
 }
 
 describe('Query', function() {
   it('rawQuery', async function() {
     const q = new Query(conn);
-    await q.rawQuery('SELECT 1+1 AS v');
+    eq(await q.rawQuery('SELECT 1+1 AS v'), [{ v:2 }]);
   });
 
   it('query(raw sql)', async function() {
     const q = new Query(conn);
-    await q.query('SELECT 1+1');
+    eq(await q.query('SELECT 1+1'), [{'1+1':2}]);
   });
 
   it('query(builder)', async function() {
     const q = new Query(conn);
     const b = q.builder();
-    await q.query(b.select(b.raw('1+1')));
+    eq(await q.query(b.select(b.raw('1+1'))), [{'1+1':2}]);
   });
 
   it('query(callback(builder))', async function() {
     const q = new Query(conn);
-    await q.query(b => b.select(b.raw('1+1')));
+    eq(await q.query(b => b.select(b.raw('1+1'))), [{'1+1':2}]);
   });
 
   it('one', async function() {
     const q = new Query(conn);
     eq(await q.query(b => b.select(b.raw('1+1')).one()), { '1+1': 2 });
+    assert.deepStrictEqual(await q.query(b => b.select().from('user').where({id:-1}).one()), null);
+  });
+
+  it('list', async function() {
+    const q = new Query(conn);
+    eq(await q.query(b => b.select().from('user').where({id:-1})), []);
   });
 
   it('count', async function() {
     const q = new Query(conn);
-    await q.count('user');
+    assert.ok(typeof await q.count('user') === 'number');
   });
 
   it('transaction', async function() {
@@ -92,18 +101,18 @@ const poolQuery = new PoolQuery(pool);
 
 describe('PoolQuery', function() {
   it('query(raw sql)', async function() {
-    await poolQuery.query('SELECT 1+1');
+    eq(await poolQuery.query('SELECT 1+1'), [{ '1+1': 2 }]);
   });
 
   it('query(builder)', async function() {
     const q = new Query(conn);
     const b = q.builder();
     b.select(b.raw('1+1'));
-    await poolQuery.query(b);
+    eq(await poolQuery.query(b), [{ '1+1': 2 }]);
   });
 
   it('query(callback(builder))', async function() {
-    await poolQuery.query(b => b.select(b.raw('1+1')));
+    eq(await poolQuery.query(b => b.select(b.raw('1+1'))), [{ '1+1': 2 }]);
   });
 
   it('transaction', async function() {
